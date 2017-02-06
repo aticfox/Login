0fb42cf5e514a9e61dbec7eaeb6a8f7a4a175c59
Kanticha Chaijiroj <Kanticha Chaijiroj>
2017-02-06 18:26:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anfield/logre/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.anfield.logre;
+
+/**
+ * Created by ANFIELD on 2/2/2560.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://asu.markgo.biz/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://asu.markgo.biz/android_login_api/register.php";
+}
